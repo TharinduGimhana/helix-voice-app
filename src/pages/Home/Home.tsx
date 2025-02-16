1538acd4f388674fa07a3f6fc9715d1f317d3c1e
THARINDU_AL2022\Tharindu Gimhana <gimhana329@gmail.com>
2025-02-16 16:06:12 +0530

    voice component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Background from "./background";
 import Microphone from "./microphone";
+import VoiceSense from "./VoiceSense";
 
 const Home: React.FC = () => {
   return <div>
     <Background />
     <Microphone />
+    <VoiceSense />
   </div>;
 };
 
---
+++ src/pages/Home/VoiceSense.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./VoiceSense.css";
+const VoiceSense: React.FC = () => {
+    return <div>
+        <div className="voice-sense-container">
+            <div className="text-container">
+                <h1>Voice Sense</h1>
+                <p>“Your Voice Reveals More Than You Think - Test 12 Vital Organs in Minutes!”</p>
+                <p>“Discover Hidden Health Insights Through Your Voice - Fast, Accurate, Science-Backed!”</p>
+                <p>“Voice Sense uses cutting-edge technology to analyze your health in seconds—no blood tests, no hassle.”</p>
+                <p>“Speak for a few seconds & get real-time insights into your body's health.”</p>
+                <p>“Get Your Voice Analysis Now - See What Your Body is Saying!”</p>
+                <p>“Try It Today - Quick, Painless, & Backed by Science!”</p>
+                <p>“Unlock Your Health Insights Now - No Appointments, No Waiting!”</p>
+            </div>
+        </div>
+    </div>;
+};
+
+export default VoiceSense;
