299657d2a45eb7f037a292f679bb494bfb5e2111
THARINDU_AL2022\Tharindu Gimhana <gimhana329@gmail.com>
2025-02-25 23:44:46 +0530

    contact us page

--- src/pages/Contact/Background.tsx
+++ src/pages/Contact/Background.tsx
@@ -3,9 +3,13 @@ import "./Background.css";
 
 const Background: React.FC = () => {
     return <div>
+        <div className="contact-header"><h2>Get in Touch, Every<br />
+            Question Has an Answer!</h2></div>
         <div className="contact-title">
-            <h2>Get in Touch, Every<br />
-                Question Has an Answer!</h2>
+            <div className="el-top"></div>
+            <div className="el-mid"> </div>
+            <div className="el-bot"></div>
+
         </div>
     </div>;
 };
