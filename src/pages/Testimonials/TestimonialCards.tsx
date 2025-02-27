d177818f313a8053c94c3bcf18fde11eee051810
THARINDU_AL2022\Tharindu Gimhana <gimhana329@gmail.com>
2025-02-27 21:47:30 +0530

    testimonial contents

--- src/pages/Testimonials/TestimonialCards.tsx
+++ src/pages/Testimonials/TestimonialCards.tsx
@@ -1,53 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TestimonialCards.css";
 import comma from "../../assets/images/testimonials/comma.png"
 
-const testimonials = [
-    {
-        text: "So I had very little pain the rest of that day! Probably a 4 later on! Was so amazing! No pain pills for a day! Thank you and Dad! You 2 are my angels!",
-        name: "Nic",
-        image: "https://randomuser.me/api/portraits/women/1.jpg",
-        accent: "blue",
-    },
-    {
-        text: "I was very stressed and a lot of anxiety when I arrived for a session. After 30 minutes my stress and anxiety had dropped from 8 down to a 1. Thank you so much.",
-        name: "Joe",
-        image: "https://randomuser.me/api/portraits/men/2.jpg",
-    },
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's...",
-        name: "Grace Scott",
-        image: "https://randomuser.me/api/portraits/women/3.jpg",
-    },
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's...",
-        name: "Antonio EVA",
-        image: "https://randomuser.me/api/portraits/men/4.jpg",
-        accent: "orange",
-    },
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's...",
-        name: "Sophia Lee",
-        image: "https://randomuser.me/api/portraits/women/5.jpg",
-    },
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's...",
-        name: "Sophia Lee",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's...",
-        name: "Olivia Miller",
-        image: "https://randomuser.me/api/portraits/women/7.jpg",
-    },
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's...",
-        name: "Sophia Lee",
-        image: "https://randomuser.me/api/portraits/women/8.jpg",
-    }
-];
+type Testimonial = {
+    text: string;
+    name: string;
+    image: string;
+    accent: string;
+};
+
 
 const TestimonialCards: React.FC = () => {
+
+    const [testimonials, seTestimonials] = useState<Testimonial[]>([]);
+
+    useEffect(() => {
+        fetch("/data/testimonials.json")
+            .then(response => response.json())
+            .then(data => seTestimonials(data))
+            .catch(error => console.error("Error loading products:", error));
+    }, []);
+
+
     return (
         <div className="testimonials-grid">
             {testimonials.map((testimonial, index) => (
