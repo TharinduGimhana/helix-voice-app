5732e624c052fb0bbe2834040ff6230a528cd052
Sachith Chamara <sachith.c@incentivio.com>
2025-02-27 21:35:59 +0530

    Changed about page content dynamically read

--- src/pages/About/MiddleSection.tsx
+++ src/pages/About/MiddleSection.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MiddleSection.css";
 
+type MiddleSectionContent = {
+  aboutUsText: string;
+  aboutContent: string;
+};
+
 const MiddleSection: React.FC = () => {
-    return <div>
-        <div className="about-section">
-            {/* Left Sidebar */}
-            <div className="about-sidebar">
-                <h2>ABOUT US</h2>
-            </div>
+  const [middleSectionContent, setMiddleSectionContent] =
+    useState<MiddleSectionContent>({
+      aboutUsText: "",
+      aboutContent: "",
+    });
 
-            {/* Main Content */}
-            <div className="about-content">
-                <p>
-                    Meet Errol Shardelow, the visionary behind Helix Scalar, VoiceSense, and VoiceNotes. With a solid foundation in Electronics Engineering and over two decades as a dedicated full-time practitioner, Errol has meticulously fine-tuned his research to develop proprietary frequency programs. These programs have demonstrated remarkable benefits in sessions as brief as 30 minutes. Imagine the possibilities when your cells are empowered to reach their full potential through the cumulative effects of regular Helix Scalar use. Experience the future of wellness with our innovative solutions
-                </p>
-            </div>
+  useEffect(() => {
+    fetch("/data/about.json")
+      .then((response) => response.json())
+      .then((data) => setMiddleSectionContent(data))
+      .catch((error) => console.error("Error loading content:", error));
+  }, []);
 
-            {/* Right Sidebar (Orange Section) */}
-            <div className="about-right"></div>
+  return (
+    <div>
+      <div className="about-section">
+        <div className="about-sidebar">
+          <h2>{middleSectionContent.aboutUsText}</h2>
         </div>
-    </div>;
+        <div className="about-content">
+          <p>{middleSectionContent.aboutContent}</p>
+        </div>
+        <div className="about-right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default MiddleSection;
